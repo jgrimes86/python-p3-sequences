5ee62934592177af09e09a43f163b2935783d581
Jim Grimes <jimgrimes86@gmail.com>
2023-10-22 09:54:55 -0400

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1,]
+    i = 1
+    while len(fibonacci) < length:
+        nextNum = fibonacci[i-1] + fibonacci[i]
+        fibonacci.append(nextNum)
+        i += 1
+    if length == 0:
+        fibonacci.clear()
+    elif length == 1:
+        del(fibonacci[1])
+    print(fibonacci)
+
+print_fibonacci(9)
